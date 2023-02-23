901da75d0afd44403fc236c9107208512d866c8a
MD ALAMIN A <86530759+Alamin0x01@users.noreply.github.com>
2023-02-23 20:53:52 +0600

    test

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ console.log(getSum(2, 7));
 // console.log(getSum(2, 7)); 
 
 
-function getSum(a, b=9){return a+b;} 
-console.log(getSum(2, 7)); 
+// function getSum(a, b=9){return a+b;} 
 // console.log(getSum(2, 7)); 
-// console.log(getSum(2, 7)); 
+// // console.log(getSum(2, 7)); 
+// // console.log(getSum(2, 7)); 
