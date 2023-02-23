fee8e349e328a624d768d5f0a44317de20ce1c68
MD ALAMIN A <86530759+Alamin0x01@users.noreply.github.com>
2023-02-23 21:56:48 +0600

    test

--- test.js
+++ test.js
@@ -8,3 +8,6 @@ console.log(getSum(2, 7));
 // console.log(getSum(2, 7)); 
 // // console.log(getSum(2, 7)); 
 // // console.log(getSum(2, 7)); 
+
+const stuInfo = JSON.stringify({ name: "James", roll: 3 }); 
+console.log(stuInfo.name); 
