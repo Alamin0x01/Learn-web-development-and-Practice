187382b8978fa46bdbc6eb9d09e79170a5b662f9
MD ALAMIN A <86530759+Alamin0x01@users.noreply.github.com>
2023-02-27 23:28:17 +0600

    show-and-hide-loading-spinner-while-loading-api-data

--- Milestone/Milestone-6 Intermediate Javascript, Api/Module-35 API Recap with phone hunter/JS/app.js
+++ Milestone/Milestone-6 Intermediate Javascript, Api/Module-35 API Recap with phone hunter/JS/app.js
@@ -36,11 +36,28 @@ const displayPhones = (phones) => {
               `;
     phoneContainer.appendChild(phoneDiv);
   });
+//   stop loader or spinner
+toggleSpinner(false);
 };
 
 document.getElementById("btn-search").addEventListener("click", function () {
+    // start loader
+    toggleSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   loadPhones(searchText);
 });
+
+const toggleSpinner = isLoading => {
+    const loaderSection =document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none')
+    }
+    else{loaderSection.classList.add('d-none');
+}
+}
+
+
+
+
 // loadPhones();
