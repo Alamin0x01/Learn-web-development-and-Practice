a436beaf033c581912519ae914c9fd11fa1b700e
MD ALAMIN A <86530759+Alamin0x01@users.noreply.github.com>
2023-03-01 09:46:18 +0600

    Block scope global scope simple understanding of Hoisting

---
+++ Milestone/Milestone-6 Intermediate Javascript, Api/Module-37 Understand-common-javascript-concepts/scope.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+  const total = a + b;
+  if (b > 5) {
+    const sum = 25 + a + b;
+  } else {
+    const sum = 5 + a + b;
+    var corrent = sum;
+  }
+  console.log(corrent);
+  return total;
+}
+// console.log(a,b);
+// console.log(total);
+add(5, 3);
