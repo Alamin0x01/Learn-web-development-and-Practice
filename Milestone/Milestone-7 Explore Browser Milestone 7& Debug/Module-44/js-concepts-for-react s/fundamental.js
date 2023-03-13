61ff92b660fa743fdaa2742f513e48e3a6ce97a8
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-03-13 19:46:26 +0600

    recon the JS

---
+++ Milestone/Milestone-7 Explore Browser Milestone 7& Debug/Module-44/js-concepts-for-react s/fundamental.js
@@ -0,0 +1,10 @@
+// 1. How to declare a variable using let and const
+const fatherName = 'fine';
+
+// Jai function er data jokhon 1 ta hobe tokhon const  -------- r ---- ar data jokhon akadik hote pare tokhon let deta korte hoi.... and var avoid korai vlo... 
+
+let season ='rainy';
+season = 'winter';
+
+
+// 
