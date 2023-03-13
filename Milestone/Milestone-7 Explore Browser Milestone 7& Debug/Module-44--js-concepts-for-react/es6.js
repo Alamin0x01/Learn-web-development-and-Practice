6c225ad67d99402a13cac5fe4be4eb925932f535
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-03-13 22:49:11 +0600

    arry functions

--- Milestone/Milestone-7 Explore Browser Milestone 7& Debug/Module-44--js-concepts-for-react/es6.js
+++ Milestone/Milestone-7 Explore Browser Milestone 7& Debug/Module-44--js-concepts-for-react/es6.js
@@ -1,13 +1,21 @@
-const numbers = [867,45,456 , 56, 76, 68];
+const numbers = [867, 45, 456, 56, 76, 68];
 
-const student ={
-    name: 'Mr. Been',
-    age:32,
-    movies: ['king man', 'super man']
+const student = {
+  name: "Mr. Been",
+  age: 32,
+  movies: ["king man", "super man"],
 };
 
 // 1. template String ......
 const about = `My Name is ${student.name} age of ${student.age} has number ${numbers[2]} also liked movies ${student.movies[1]} `;
-console.log(about)
+console.log(about);
 
-
+// 2 . array function
+const getFiftyFive = () => 55;
+const addSixtyFive = (num) => num + 65;
+const isEven = (x) => x % 2 == 0;
+const addTree = (x, y, z) => x + y + z;
+const doMath = (num1, num2) => {
+  const sum = num1 + num2;
+  return sum;
+};
--- Milestone/Milestone-7 Explore Browser Milestone 7& Debug/Module-44--js-concepts-for-react/fundamental.js
+++ Milestone/Milestone-7 Explore Browser Milestone 7& Debug/Module-44--js-concepts-for-react/fundamental.js
@@ -1,73 +1,59 @@
 // 1. How to declare a variable using let and const
-const fatherName = 'fine';
+const fatherName = "fine";
 
-// Jai function er data jokhon 1 ta hobe tokhon const  -------- r ---- ar data jokhon akadik hote pare tokhon let deta korte hoi.... and var avoid korai vlo... 
+// Jai function er data jokhon 1 ta hobe tokhon const  -------- r ---- ar data jokhon akadik hote pare tokhon let deta korte hoi.... and var avoid korai vlo...
 
-let season ='rainy';
-season = 'winter';
-
+let season = "rainy";
+season = "winter";
 
 // 2. conditions
 // 6 basic conditions : > , <, ===, !==,<=, >=
 // multiple conditions : &&, ||
-if (fatherName === 'fine' || season === 'rainy'){
-
+if (fatherName === "fine" || season === "rainy") {
+} else if (fatherName === "Fine") {
+} else {
 }
-else if (fatherName === 'Fine'){
-
-}
-else{
-
-}
-
-
 
 // 3. Array declare
 // index,
 // length, push,
-const numbers = [867,45,456 , 56, 76, 687,];
+const numbers = [867, 45, 456, 56, 76, 687];
 numbers[0] = 54;
 
-
-
-
-
 //  4. for loop
-for (let i = 0; i < numbers.length; i++){
-    const number =numbers[i];
-    // console.log(number);
+for (let i = 0; i < numbers.length; i++) {
+  const number = numbers[i];
+  // console.log(number);
 }
-
-
-
-
 
 // 5 . function
 
-function multiple(num1, num2){
-    const result =num1 *num2;
-    return result;
+function multiple(num1, num2) {
+  const result = num1 * num2;
+  return result;
 }
 
 const output = multiple(34, 87);
 
-
-
 // 6. object
 //  3 ways to access property by name
-const student ={
-    name: 'Mr. Been',
-    age:32,
-    movies: ['king man', 'super man']
-}
+const student = {
+  name: "Mr. Been",
+  age: 32,
+  movies: ["king man", "super man"],
+};
 
-const myVariable = 'age';
+const myVariable = "age";
 
 // direct by property---
-console.log(student.age); 
+console.log(student.age);
+
+
+
+
 
 // access via property name string
-console.log(student['age']);
+console.log(student["age"]);
 
 // access via property name in a variable
 console.log(student[myVariable]);
