a325c3c7465a8470a957fbfeb22f8a46643ed674
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-03-14 00:10:00 +0600

    filter and find

--- Milestone/Milestone-7 Explore Browser Milestone 7& Debug/Module-44--js-concepts-for-react/array-methods.js
+++ Milestone/Milestone-7 Explore Browser Milestone 7& Debug/Module-44--js-concepts-for-react/array-methods.js
@@ -8,11 +8,11 @@ const products = [
 
 const brands = products.map((product) => product.brand);
 
-console.log(brands);
+// console.log(brands);
 
 const prices = products.map((product) => product.price);
 
-console.log(prices);
+// console.log(prices);
 
 // 2. forEach
 products.forEach((product) => console.log(product));
@@ -23,3 +23,14 @@ products.forEach((product) => {
 });
 
 // 3. filter
+const cheap = products.filter((product) => product.price <= 5000);
+// console.log(cheap);
+
+const specificName = products.filter((p) => p.name.includes("n"));
+// console.log(specificName);
+
+// 4. find
+
+const special = products.find((p) => p.name.includes("n"));
+
+console.log(special);
