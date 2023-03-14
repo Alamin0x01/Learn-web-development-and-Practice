b3d5eb033908e669b7c8ba4ede83d7248866ab8f
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-03-14 15:43:17 +0600

    notation

---
+++ Milestone/Milestone-7 Explore Browser Milestone 7& Debug/Module-44--js-concepts-for-react/notation.js
@@ -0,0 +1,23 @@
+const person ={
+    name:'lal mia',
+    profession: 'traffic surgeon',
+    age: 32,
+    22:'summer',
+    'son-name': 'sunny',
+    address:'shahabag'
+}
+
+// dot notation 
+const prof1 = person.profession;
+
+
+// bracket notation
+const prof2 = person ["profession"];
+const pName ='profession';
+const prof3 =person[pName];
+// console.log(prof3);
+
+const season =person[25];
+// console.log(season);
+const son =person['son-name'];
+console.log(son);
