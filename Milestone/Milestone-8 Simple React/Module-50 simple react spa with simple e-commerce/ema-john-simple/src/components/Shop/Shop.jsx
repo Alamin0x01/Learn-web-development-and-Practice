0f2559cedeb3a7103c689e384ec413a25d7e6960
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-03-28 12:15:54 +0600

    ema-jhon-simple project

---
+++ Milestone/Milestone-8 Simple React/Module-50 simple react spa with simple e-commerce/ema-john-simple/src/components/Shop/Shop.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
+    const handleAddToCart = (product) => {
+        // cart.push(product); 
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                    ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <h4>Order Summary</h4>
+                <p>Selected Items: {cart.length}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
